498fb31a919bbbcc7adaa5ae6be8c2795e29f86d
NightJuan <juancerritos69@gmail.com>
2026-04-27 12:03:43 -0500

    Created the first scene

--- practice-c-rpg/global.gd
+++ practice-c-rpg/global.gd
@@ -3,3 +3,17 @@ extends Node
 var class_choice = "null"
 var turn_order = 0
 var input = ""
+var fighter = ""
+
+func reward_one(role):
+	# Convert the input to lowercase before checking
+	var r = role.to_lower()
+	
+	if r == "fighter":
+		return "Great Sword"
+	elif r == "ranger":
+		return "Reinforced Bow"
+	elif r == "wizard":
+		return "Great Staff"
+	
+	return "Unknown Item" # Good practice to have a fallback
--- practice-c-rpg/src/main.gd
+++ practice-c-rpg/src/main.gd
@@ -2,6 +2,7 @@ extends Control
 
 signal	entered_pressed
 signal	class_selected
+signal	stage_ended
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,12 +10,12 @@ func _ready() -> void:
 	display_text("Welcome Travelor, Choose a Class: \n	1. Fighter\n	2. Ranger\n	3. Wizard")
 	
 	await entered_pressed
-	class_selection(global.input)
+	class_selection_flow()
 	
 	await class_selected
-	display_text("You have Chossen a {c}".format({"c":global.class_choice}))
-	await get_tree().create_timer(3.0).timeout
-	display_text("")
+	display_text("You have Chossen a {c}\n Great Choice".format({"c":global.class_choice}))
+	await get_tree().create_timer(2.0).timeout
+	stage_one_flow()
 
 func display_text(text):
 	$VBoxContainer/Message.text = text
@@ -23,33 +24,57 @@ func input_grabber():
 	global.input = $VBoxContainer/Input.text
 	$VBoxContainer/Input.text = ""
 
-func class_selection(text_input):
-	
-	var current_input = text_input.strip_edges()
+func class_selection_flow():
 	while true:
-		if current_input == "1" or current_input.to_lower() == "fighter":
+		await entered_pressed
+		var choice = global.input.to_lower()
+		
+		if choice in ["1", "fighter"]:
 			global.class_choice = "Fighter"
-			print("fighter")
 			break
-		elif current_input == "2" or current_input.to_lower() == "ranger":
+		elif choice in ["2", "ranger"]:
 			global.class_choice = "Ranger"
-			print("ranger")
 			break
-		elif current_input == "3" or current_input.to_lower() == "wizard":
+		elif choice in ["3", "wizard"]:
 			global.class_choice = "Wizard"
-			print("wizard")
 			break
 		else:
-			$VBoxContainer/Input.placeholder_text = "NOT A CHOICE"
-			await get_tree().create_timer(1.0).timeout
-			$VBoxContainer/Input.placeholder_text = "Type Anything"
-		await entered_pressed
-		current_input = global.input.strip_edges()
-	await get_tree().process_frame # Waits 1 frame so _ready can start its await
-	class_selected.emit()
-	print("class_selection ended")
+			show_error_feedback()
 
-func _input(event):
+func stage_one_flow():
+	display_text("You have been chosen by the KING to slay the GOBLIN KING.\n" +
+				 "As you travel, you spot a flipped Merchant Wagon.\n" +
+				 "What do you do:\n1. Help the Merchant\n2. Steal the loot")
+	
+	while true:
+		await entered_pressed
+		var choice = global.input.to_lower()
+		
+		if choice in ["1", "help", "help them"]:
+			event_one()
+			break
+		elif choice in ["2", "steal", "steal the loot"]:
+			battle_one()
+			break
+		else:
+			show_error_feedback()
+	
+	stage_ended.emit()
+
+func show_error_feedback():
+	$VBoxContainer/Input.placeholder_text = "NOT A CHOICE"
+	await get_tree().create_timer(1.0).timeout
+	$VBoxContainer/Input.placeholder_text = "Type Anything"
+
+func event_one():
+	var reward = global.reward_one(global.class_choice)
+	display_text("You help the merchant and he gives you a {r}".format({"r": reward}))
+
+func battle_one():
+	pass
+
+
+func _input(_event):
 	if (Input.is_action_just_pressed("ui_text_newline") and not $VBoxContainer/Input.text.is_empty()):
 		input_grabber()
 		entered_pressed.emit()
